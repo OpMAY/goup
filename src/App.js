fd2b598d084e32fc5d37550420213b7dff8c4b35
YunHyunHo <roal3437@naver.com>
2023-02-28 13:37:33 +0900

    common 작업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import {Routes, Route} from 'react-router-dom';
-import Kakao from "./module/Kakao";
-import axios from "axios";
+// import Kakao from "./module/Kakao";
+// import axios from "axios";
+import {Button} from "./common/style"
 
 
 function App() {
-    const kakaoTest = () => {
-        axios.post('http://localhost:8080/api/sns/key/kakao', {}).then((res) => {
-            const data = res.data;
-            if (data.status === 'OK') {
-                const clientId = data.data.key;
-                const redirectURI = 'http://localhost:3000/oauth';
-                window.location.href = 'https://kauth.kakao.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirectURI + '&response_type=code'
-            }
-        })
-    }
+    // const kakaoTest = () => {
+    //     axios.post('http://localhost:8080/api/sns/key/kakao', {}).then((res) => {
+    //         const data = res.data;
+    //         if (data.status === 'OK') {
+    //             const clientId = data.data.key;
+    //             const redirectURI = 'http://localhost:3000/oauth';
+    //             window.location.href = 'https://kauth.kakao.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirectURI + '&response_type=code'
+    //         }
+    //     })
+    // }
 
     return (
         <div className="App">
+            <Button type="button" height="40px;">버튼</Button>
             <Routes>
                 {/*<Route path="" element={}/>*/}
-                <Route path="/oauth" element={<Kakao/>}/>
+                {/* <Route path="/oauth" element={<Kakao/>}/> */}
             </Routes>
         </div>
     );
--- src/common/style.js
+++ src/common/style.js
@@ -1,1 +1,9 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    height: ${props => props.height}
+    border: 1px solid #d3d3d3;
+    border-radius: 12px;
+    padding: 10px 30px;
+    background-color: 
+`
