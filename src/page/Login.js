8b1f0a419b2328cc8c744ca1a7102dc023464a66
HarryA123 <harrykwak123@gmail.com>
2023-03-07 16:56:11 +0900

    로그인 페이지 생성

---
+++ src/page/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "../component/Login/LoginPage";
+
+const Login = () => {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+};
+
+export default Login;
