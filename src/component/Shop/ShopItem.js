5a17fa4577e53fa24a56a1c47f08f594a3f91e56
한지우 <kevin@hanjiuui-MacBookPro.local>
2023-03-24 16:48:42 +0900

    state change

--- src/component/Shop/ShopItem.js
+++ src/component/Shop/ShopItem.js
@@ -1,58 +1,67 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import { productAtom } from '../../atoms/atom';
-import { useRecoilState } from 'recoil';
+import {productAtom} from '../../atoms/atom';
+import {useRecoilState} from 'recoil';
 
 const ItemBlcok = styled.div`
-  p{
-    margin:0;
+  p {
+    margin: 0;
   }
-  .product-info{
+
+  .product-info {
     padding: 8px 4px 0;
     margin-bottom: 12px;
     height: 71px;
-    .name{
+
+    .name {
       font-size: 13px;
       color: #333;
       font-weight: 700;
       margin-bottom: 2px;
     }
-    .item-detail{
+
+    .item-detail {
       font-size: 13px;
       color: #222;
       margin-bottom: 2px;
     }
-    .item-detail-kr{
+
+    .item-detail-kr {
       font-size: 11px;
-      color: rgba(34,34,34,.5);
+      color: rgba(34, 34, 34, .5);
     }
   }
-  .price-info{
+
+  .price-info {
     padding: 0 4px;
-    .price{
+
+    .price {
       font-size: 14px;
       font-weight: 700;
     }
-    .desc{
+
+    .desc {
       font-size: 11px;
-      color: rgba(34,34,34,.5);
+      color: rgba(34, 34, 34, .5);
     }
   }
-  
-  .icon-box{
+
+  .icon-box {
     padding: 12px 4px 0 0;
     display: flex;
     justify-content: space-between;
-    .save{
+
+    .save {
 
     }
-    .save{
+
+    .save {
       cursor: pointer;
     }
   }
-  
+
 `
 
 const ImgBox = styled.div`
@@ -63,37 +72,42 @@ const ImgBox = styled.div`
 `
 
 const ShopItem = ({product, idx}) => {
-  console.log(product)
-  const toggle = product._wish;
-  const [products, setProduct] = useRecoilState(productAtom)
+    const toggle = product._wish;
+    const [products, setProducts] = useRecoilState(productAtom)
 
-  const controlToggle = () => {
-    
-  }
+    const controlToggle = () => {
+        const sample = {...product};
+        sample._wish = !sample._wish;
+        const newList = [...products].map((v, i) => {
+            if (i === idx) return sample; else return v;
+        })
+        setProducts(newList);
+        console.log('controlToggle')
+    }
 
-  return (
-    <ItemBlcok>
-      <ImgBox backgroundImage={product.image.url}></ImgBox>
-      <div>
-        <div className='product-info'>
-          <p className='name'>{product.brand.name}</p>
-          <p className='item-detail'>{product.en_name}</p>
-          <p className='item-detail-kr'>{product.kor_name}</p>
-        </div>
-        <div className='price-info'>
-          <p className='price'>{product.price} 원</p>
-          <p className='desc'>즉시 구매가</p>
-        </div>
-        <div className='icon-box'>
-          <div className='save' onClick={controlToggle}>
-            { 
-             !toggle ? <BookmarkBorderIcon /> : <BookmarkIcon/>
-            }
-          </div>
-        </div>
-      </div>
-    </ItemBlcok>
-  )
+    return (
+        <ItemBlcok>
+            <ImgBox backgroundImage={product.image.url}></ImgBox>
+            <div>
+                <div className='product-info'>
+                    <p className='name'>{product.brand.name}</p>
+                    <p className='item-detail'>{product.en_name}</p>
+                    <p className='item-detail-kr'>{product.kor_name}</p>
+                </div>
+                <div className='price-info'>
+                    <p className='price'>{product.price} 원</p>
+                    <p className='desc'>즉시 구매가</p>
+                </div>
+                <div className='icon-box'>
+                    <div className='save' onClick={controlToggle}>
+                        {
+                            !toggle ? <BookmarkBorderIcon/> : <BookmarkIcon/>
+                        }
+                    </div>
+                </div>
+            </div>
+        </ItemBlcok>
+    )
 }
 
 export default ShopItem
