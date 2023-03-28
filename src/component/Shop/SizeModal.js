97b56043d2ed5ad8a77285633e4fdf44586e45ce
한지우 <kevin@hanjiuui-MacBookPro.local>
2023-03-28 17:45:08 +0900

    wish connect with server:

--- src/component/Shop/SizeModal.js
+++ src/component/Shop/SizeModal.js
@@ -6,7 +6,7 @@ import Modal from '@mui/material/Modal'
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import {useRecoilState, useRecoilValue} from 'recoil';
-import {modalOpenAtom, modalProductAtom, productAtom, sizeAtom} from '../../atoms/atom'
+import {modalOpenAtom, modalProductAtom, productAtom, sizeAtom, tokenAtom} from '../../atoms/atom'
 import {axiosPostFunction} from "../../module/CustomAxios";
 
 const Box = styled.div`
@@ -105,6 +105,7 @@ const SizeModal = () => {
     const [sizes, setSizes] = useRecoilState(sizeAtom);
     const [checkBtn, setCheckBtn] = useState(false);
     const [products, setProducts] = useRecoilState(productAtom);
+    const [token, setToken] = useRecoilState(tokenAtom);
 
     const handleClose = () => {
         setOpen(false)
@@ -129,22 +130,32 @@ const SizeModal = () => {
 
     const handleConfirm = () => {
         // 통신
-        // axiosPostFunction().then((res) => {
-        //     if(res.data.status === 'OK') {
-        //
-        //     }
-        // })
-        const result = sizes.findIndex((element, index, array) => {
+        const body = {}
+        body.user_no = 1;
+        body.product_no = modalProduct.no;
+        const result = sizes.filter((element, index, array) => {
             return element._wish === true
-        }) !== -1;
-        console.log(modalProduct);
-        const this_product = {...modalProduct};
-        this_product._wish = result;
-        const productFormattedList = [...products].map(product => {
-            if(this_product.no === product.no) return this_product; else return product;
         });
-        setProducts(productFormattedList);
-        handleClose();
+        const wishes = [];
+        result.map(size => {
+            wishes.push({
+                size_no : size.no,
+                user_no : body.user_no
+            })
+        })
+        body.wishes = wishes;
+        axiosPostFunction('/api/kream/product/wish', body, false, token, setToken).then((res) => {
+            console.log(res);
+            if(res.data.status === 'OK') {
+                const this_product = {...modalProduct};
+                this_product._wish = res.data.data.status;
+                const productFormattedList = [...products].map(product => {
+                    if(this_product.no === product.no) return this_product; else return product;
+                });
+                setProducts(productFormattedList);
+                handleClose();
+            }
+        })
     }
     return (
         <>
