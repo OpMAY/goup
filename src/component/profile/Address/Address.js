06805414096684ef27aac42ddb7e7afd366ad65f
HarryA123 <harrykwak123@gmail.com>
2023-04-19 18:50:04 +0900

    주소록 API

--- src/component/profile/Address/Address.js
+++ src/component/profile/Address/Address.js
@@ -1,334 +1,142 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import Modal from '@mui/material/Modal';
-import { Checkbox, FormControlLabel } from "@mui/material";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import {Box} from "@mui/material"
+import Chip from "@mui/material/Chip";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { tokenAtom, userAtom, userAddressAtom } from "../../../atoms/atom";
+import {
+  axiosGetFunction,
+  axiosPutFunction,
+} from "../../../module/CustomAxios";
+import AddAddressModal from "../../modal/AddAddressModal";
+import PutAddressModal from "../../modal/PutAddressModal";
+import DeleteAddressModal from "../../modal/DeleteAddressModal";
 
 const AddressBlcok = styled.div`
-  .address_list{
+  .title_box {
+    display: flex;
+    justify-content: space-between;
+    .title {
+      font-size: 24px;
+      color: #000;
+      font-weight: bold;
+      display: inline;
+    }
+  }
+  .address_list {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 30px 0;
     border-bottom: 2px solid #000;
+    .empty_address_notice {
+      margin: auto;
+    }
 
-    &.additional{
+    &.additional {
       border-bottom: 1px solid #d3d3d3;
     }
-    .btn{
-      display: inline-flex;
-      height: 34px;
-      background-color: #fff;
-      align-items: center;
-      color:rgba(34,34,34,.8);
-      border: 1px solid #d3d3d3;
-      font-size: 12px;
-      border-radius: 10px;
-      padding: 0 12px;
-      cursor: pointer;
-
-      &:last-child{
-        margin-left: 10px;
-      }
+    .btn_grop {
+      display: flex;
+      gap: 10px;
     }
-
   }
-  .address_info{
-    .name{
+  .address_info {
+    .name {
       font-size: 15px;
       font-weight: 600;
       vertical-align: middle;
     }
-    .chip{
+    .chip {
       height: 20px;
       display: inline-flex;
       background-color: #f4f4f4;
       color: #222;
       font-size: 12px;
       align-items: center;
-      padding:0 6px;
+      padding: 0 6px;
       margin-left: 5px;
       border-radius: 15px;
     }
-    .phone{
+    .phone {
       font-size: 15px;
-      margin: 5px 0 0 0 ;
+      margin: 5px 0 0 0;
     }
-    .address_box{
-      margin: 5px 0 0 0 ;
+    .address_box {
+      margin: 5px 0 0 0;
       font-size: 14px;
     }
-  }
-`
-
-const TitleBlock = styled.div`
-  display: flex;
-  justify-content: space-between;
-  
-  .title{
-    font-size: 24px;
-    color: #000;
-    font-weight: bold;
-  }
-  .btn{
-    display: inline-flex;
-    height: 34px;
-    align-items: center;
-    padding: 0 14px;
-    font-size: 12px;
-    background-color: #fff;
-    border-radius: 10px;
-    border: 1px solid #000;
-    cursor: pointer;
-  }
-`
-const ModalBox = styled.div`
-  width: 520px;
-  border-radius: 10px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  box-shadow: 24;
-  z-index: 9;
-  background-color: #fff;
-  .title{
-    padding: 20px 50px;
-    font-size: 18px;
-    text-align: center;
-  }
-  .modal_body{
-    padding: 0 32px ;
-  }
-  .btn_box{
-    text-align: center;
-    padding: 24px 32px 32px;
-    .btn{
-      min-width: 120px;
-      height: 42px;
-      display: inline-flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 14px;
-      font-weight: 700;
-      border-radius: 10px;
-
-      &.btn_back{
-        background-color: #fff;
-        border: 1px solid #ccc;
-        color: #000;
-      }
-      &.btn_save{
-        background-color: #222;
-        color: #fff;
-        border: 0;
-        margin-left: 6px;
-      }
-    }
-  }
-  .modal_input_box{
-    padding: 10px 0 14px;
-    &:first-child{
-      padding-top: 0;
-    }
-    .input_title{
-      font-size: 13px;
-      color: #222;
-      margin: 0;
-    }
-    .modal_input{
-      width: 100%;
-      border: 0;
-      border-bottom: 1px solid #ccc;
-      outline: none;
-      padding: 8px 0;
-      font-size: 15px;
-
-      &:focus{
-        border-bottom: 1px solid #000;
-      }
-    }
-  }
-`
-const CheckAreaBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items:center;
-  div {
-    p {
-      margin: 0;
-    }
-    .main_text {
-      color: #222;
-      font-size: 15px;
-      line-height: 18px;
-    }
-    .sub_text {
-      margin-top: 4px;
-      color: rgba(34, 34, 34, 0.5);
-      font-size: 13px;
-      line-height: 16px;
-    }
-  }
-  span {
   }
 `;
+
 const Address = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const [open2, setOpen2] = useState(false);
-  const handleOpen2 = () => setOpen(true);
-  const handleClose2 = () => setOpen(false);
+  const [token, setToken] = useRecoilState(tokenAtom);
+  const user = useRecoilValue(userAtom);
+  const [userAddress, setUserAddress] = useRecoilState(userAddressAtom);
+
+  useEffect(() => {
+    axiosGetFunction(`/api/kream/my/address/` + user, {}, token, setToken).then(
+      res => {
+        setUserAddress(res.data.data.address);
+      }
+    );
+  }, []);
 
   return (
     <>
-      <TitleBlock>
-        <div>
-          <h3 className='title'>주소록</h3>
-        </div>
-        <div>
-          <button type='button' className='btn' onClick={handleOpen}>+ 새 배송지 추가</button>
-        </div>
-      </TitleBlock>
       <AddressBlcok>
-        <div className='address_list'>
-          <div className='address_info'>
-            <div>
-              <span className='name'>윤현호</span>
-              <div className='chip'>
-                <span>기본 배송지</span>
+        <div className="title_box">
+          <h3 className="title">주소록</h3>
+          <AddAddressModal />
+        </div>
+        {userAddress !== undefined ? (
+          userAddress.map(item => (
+            <div className="address_list" key={item.no}>
+              <div className="address_info">
+                <div>
+                  <span className="name">{item.name}</span>
+                  <div className="chip">
+                    {item._default_address ? (
+                      <Box
+                        component="span"
+                        sx={{
+                          display: "inline-block",
+                          verticalAlign: "top",
+                          lineHeight: "14px",
+                          marginTop: "0",
+                          marginLeft: "4px",
+                          padding: "3px 6px",
+                          fontSize: "12px",
+                          letterSpacing: "-.06px",
+                          borderRadius: "10px",
+                          backgroundColor: "#f4f4f4",
+                          boxSizing: "border-box",
+                        }}>
+                        기본 배송지
+                      </Box>
+                    ) : null}
+                  </div>
+                </div>
+                <p className="phone">{item.phone_number}</p>
+                <p className="address_box">
+                  <span className="zipcode">({item.zip_code})</span>
+                  <span className="adress">{item.address}</span>
+                </p>
+              </div>
+              <div className="btn_grop">
+                <PutAddressModal addressInfo={item} />
+                <DeleteAddressModal addressInfo={item} />
               </div>
             </div>
-            <p className='phone'>010-0000-0000</p>
-            <p className='address_box'>
-              <span className='zipcode'>(00000)</span>
-              <span className='adress'>용마산로 36길 15-16</span>
-            </p>
+          ))
+        ) : (
+          <div className="address_list">
+            <h3 className="empty_address_notice"> 등록된 주소가 없습니다</h3>
           </div>
-          <div className='btn_grop'>
-            <button type='button' className='btn' onClick={handleOpen2}>수정</button>
-            <button type='button' className='btn'>삭제</button>
-          </div>
-        </div>
-        <div className='address_list additional'>
-          <div className='address_info'>
-            <div>
-              <span className='name'>윤현호</span>
-              <div className='chip'>
-                <span>기본 배송지</span>
-              </div>
-            </div>
-            <p className='phone'>010-0000-0000</p>
-            <p className='address_box'>
-              <span className='zipcode'>(00000)</span>
-              <span className='adress'>용마산로 36길 15-16</span>
-            </p>
-          </div>
-          <div className='btn_grop'>
-            <button type='button' className='btn' onClick={handleOpen2}>수정</button>
-            <button type='button' className='btn'>삭제</button>
-          </div>
-        </div>
+        )}
       </AddressBlcok>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <ModalBox>
-          <h2 className='title'>세 주소 추가</h2>
-          <div className='modal_body'>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>이름</h4>
-              <input type="text" placeholder='수령인의 이름' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>휴대폰 번호</h4>
-              <input type="text" placeholder='- 없이 입력하세요.' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>우편번호</h4>
-              <input type="text" placeholder='우편 번호를 검색하세요.' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>주소</h4>
-              <input type="text" placeholder='우편 번호 검색 후, 자동입력 됩니다.' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>상세 주소</h4>
-              <input type="text" placeholder='건물, 아파트, 동/호수 입력하세요.' className='modal_input'></input>
-            </div>
-            <CheckAreaBox>
-              <FormControlLabel control={ <Checkbox
-                sx={{
-                  "& .MuiSvgIcon-root": { fontSize: 28 },
-                  color: "#ebebeb",
-                  "&.Mui-checked": {
-                    color: "black",
-                  },
-                }}
-              />} label="기본 배송지로 설정" />
-             
-            </CheckAreaBox>
-          </div>
-          <div className='btn_box'>
-            <button className='btn btn_back' onClick={() => setOpen(false)}>취소</button>
-            <button className='btn btn_save'>저장하기</button>
-          </div>
-        </ModalBox>
-      </Modal>
-      <Modal
-        open={open2}
-        onClose={handleClose2}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <ModalBox>
-          <h2 className='title'>배송지 수정</h2>
-          <div className='modal_body'>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>이름</h4>
-              <input type="text" placeholder='수령인의 이름' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>휴대폰 번호</h4>
-              <input type="text" placeholder='- 없이 입력하세요.' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>우편번호</h4>
-              <input type="text" placeholder='우편 번호를 검색하세요.' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>주소</h4>
-              <input type="text" placeholder='우편 번호 검색 후, 자동입력 됩니다.' className='modal_input'></input>
-            </div>
-            <div className='modal_input_box '>
-              <h4 className='input_title'>상세 주소</h4>
-              <input type="text" placeholder='건물, 아파트, 동/호수 입력하세요.' className='modal_input'></input>
-            </div>
-            <CheckAreaBox>
-              <FormControlLabel control={ <Checkbox
-                sx={{
-                  "& .MuiSvgIcon-root": { fontSize: 28 },
-                  color: "#ebebeb",
-                  "&.Mui-checked": {
-                    color: "black",
-                  },
-                }}
-                Checked
-              />} label="기본 배송지로 설정" />
-             
-            </CheckAreaBox>
-          </div>
-          <div className='btn_box'>
-            <button className='btn btn_back' onClick={() => setOpen2(false)}>취소</button>
-            <button className='btn btn_save'>저장하기</button>
-          </div>
-        </ModalBox>
-      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default Address
+export default Address;
--- src/module/CustomAxios.js
+++ src/module/CustomAxios.js
@@ -1,99 +1,208 @@
 import axios from "axios";
 
-const DEFAULT_SERVER_URL = 'http://43.200.202.87';
+const DEFAULT_SERVER_URL = "http://43.200.202.87";
 // const DEFAULT_SERVER_URL = 'http://localhost:8080';
-const DEFAULT_ACCESS_KEY = 'N4gdubumGsrvzFFzewu4hQ==';
+const DEFAULT_ACCESS_KEY = "N4gdubumGsrvzFFzewu4hQ==";
 
 export const setDefaultAxios = () => {
-    console.log("setting axios defaults...");
-    axios.defaults.url = DEFAULT_SERVER_URL;
-    axios.defaults.headers.common['Access-Control-Allow-Credentials'] = true;
-    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-    axios.defaults.headers.common['Access-Control-Expose-Headers'] = '*';
-    console.log("setting axios defaults done");
-}
+  console.log("setting axios defaults...");
+  axios.defaults.url = DEFAULT_SERVER_URL;
+  axios.defaults.headers.common["Access-Control-Allow-Credentials"] = true;
+  axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+  axios.defaults.headers.common["Access-Control-Expose-Headers"] = "*";
+  console.log("setting axios defaults done");
+};
 
 const authFunction = async (token, setToken) => {
-    console.log('auth token setting...')
-    return new Promise((resolve, reject) => {
-        axios.get(DEFAULT_SERVER_URL + '/api/auth', {
-            params: {
-                key: DEFAULT_ACCESS_KEY
-            }
-        }).then((res) => {
-            if (res.data.status === 'OK') {
-                setToken(res.data.data.key);
-                console.log('auth token set Complete')
-                resolve(res)
-            } else {
-                reject(res)
-            }
-        })
-    })
-}
+  console.log("auth token setting...");
+  return new Promise((resolve, reject) => {
+    axios
+      .get(DEFAULT_SERVER_URL + "/api/auth", {
+        params: {
+          key: DEFAULT_ACCESS_KEY,
+        },
+      })
+      .then(res => {
+        if (res.data.status === "OK") {
+          setToken(res.data.data.key);
+          console.log("auth token set Complete");
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      });
+  });
+};
 
-export const axiosGetFunction = async (url, params, token, setToken, additional_header = null) => {
-    const config = {};
-    config.headers = {
-        contentType: 'application/x-www-form-urlencoded',
-        authorization: 'bearer ' + token
-    }
-    if(additional_header) {
-        Object.keys(additional_header).forEach(key => {
-            config.headers[key] = additional_header[key];
+export const axiosGetFunction = async (
+  url,
+  params,
+  token,
+  setToken,
+  additional_header = null
+) => {
+  const config = {};
+  config.headers = {
+    contentType: "application/x-www-form-urlencoded",
+    authorization: "bearer " + token,
+  };
+  if (additional_header) {
+    Object.keys(additional_header).forEach(key => {
+      config.headers[key] = additional_header[key];
+    });
+  }
+  config.params = params;
+  return new Promise((resolve, reject) => {
+    axios.get(DEFAULT_SERVER_URL + url, config).then(async res => {
+      if (res.data.status === "UNAUTHORIZED") {
+        await authFunction(token, setToken).then(() => {
+          config.headers["authorization"] = "bearer " + token;
+          axios.get(DEFAULT_SERVER_URL + url, config).then(res1 => {
+            console.log("response : ", res1);
+            resolve(res1);
+          });
         });
-    }
-    config.params = params;
-    return new Promise((resolve, reject) => {
-        axios.get(DEFAULT_SERVER_URL + url, config).then(async (res) => {
-            if (res.data.status === 'UNAUTHORIZED') {
-                await authFunction(token, setToken).then(() => {
-                    config.headers['authorization'] = 'bearer ' + token;
-                    axios.get(DEFAULT_SERVER_URL + url, config).then((res1) => {
-                        console.log('response : ',res1);
-                        resolve(res1)
-                    })
-                })
-            } else if (res.data.status === 'OK') {
-                console.log('request success params : ', params);
-                console.log('response on GET', url, ' : ',res);
-                resolve(res);
-            } else {
-                reject(res);
-            }
-        })
-    })
-}
+      } else if (res.data.status === "OK") {
+        console.log("request success params : ", params);
+        console.log("response on GET", url, " : ", res);
+        resolve(res);
+      } else {
+        reject(res);
+      }
+    });
+  });
+};
 
-export const axiosPostFunction = async (url, formData, hasFile, token, setToken) => {
+export const axiosPostFunction = async (
+  url,
+  formData,
+  hasFile,
+  token,
+  setToken
+) => {
+  const config = {};
+  config.headers = {
+    contentType: "application/x-www-form-urlencoded",
+    authorization: "bearer " + token,
+  };
+  if (hasFile === true) {
+    config.headers.contentType = "multipart/form-data";
+  }
+  console.log(config);
+  return new Promise((resolve, reject) => {
+    axios
+      .post(DEFAULT_SERVER_URL + url, formData, config)
+      .then(async res => {
+        if (res.data.status === "UNAUTHORIZED") {
+          await authFunction(token, setToken).then(() => {
+            console.log(config);
+            config.headers["authorization"] = "bearer " + token;
+            axios
+              .post(DEFAULT_SERVER_URL + url, formData, config)
+              .then(res2 => {
+                console.log("response on POST", url, " : ", res);
+                resolve(res2);
+              });
+          });
+        } else if (res.data.status === "OK") {
+          console.log("response on POST", url, " : ", res);
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      })
+      .catch(reject => {
+        console.log(reject);
+      });
+  });
+};
+
+export const axiosPutFunction = async (
+  url,
+  formData,
+  hasFile,
+  token,
+  setToken
+) => {
+  const config = {};
+  config.headers = {
+    contentType: "application/x-www-form-urlencoded",
+    authorization: "bearer " + token,
+  };
+  if (hasFile === true) {
+    config.headers.contentType = "multipart/form-data";
+  }
+  console.log(config);
+  return new Promise((resolve, reject) => {
+    axios
+      .put(DEFAULT_SERVER_URL + url, formData, config)
+      .then(async res => {
+        if (res.data.status === "UNAUTHORIZED") {
+          await authFunction(token, setToken).then(() => {
+            console.log(config);
+            config.headers["authorization"] = "bearer " + token;
+            axios
+              .put(DEFAULT_SERVER_URL + url, formData, config)
+              .then(res2 => {
+                console.log("response on PUT", url, " : ", res);
+                resolve(res2);
+              });
+          });
+        } else if (res.data.status === "OK") {
+          console.log("response on PUT", url, " : ", res);
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      })
+      .catch(reject => {
+        console.log(reject);
+      });
+  });
+};
+
+
+export const axiosDeleteFunction = async (
+    url,
+    formData,
+    hasFile,
+    token,
+    setToken
+  ) => {
     const config = {};
     config.headers = {
-        contentType: 'application/x-www-form-urlencoded',
-        authorization: 'bearer ' + token
-    }
+      contentType: "application/x-www-form-urlencoded",
+      authorization: "bearer " + token,
+    };
     if (hasFile === true) {
-        config.headers.contentType = 'multipart/form-data'
+      config.headers.contentType = "multipart/form-data";
     }
     console.log(config);
     return new Promise((resolve, reject) => {
-        axios.post(DEFAULT_SERVER_URL + url, formData, config).then(async (res) => {
-            if (res.data.status === 'UNAUTHORIZED') {
-                await authFunction(token, setToken).then(() => {
-                    console.log(config)
-                    config.headers['authorization'] = 'bearer ' + token;
-                    axios.post(DEFAULT_SERVER_URL + url, formData, config).then((res2) => {
-                        console.log('response on POST', url, ' : ',res);
-                        resolve(res2);
-                    })
-                })
-            } else if (res.data.status === 'OK') {
-                console.log('response on POST', url, ' : ',res);
-                resolve(res);
-            } else {
-                reject(res);
-            }
-        }).catch((reject) => {
-            console.log(reject);
+      axios
+        .delete(DEFAULT_SERVER_URL + url, formData, config)
+        .then(async res => {
+          if (res.data.status === "UNAUTHORIZED") {
+            await authFunction(token, setToken).then(() => {
+              console.log(config);
+              config.headers["authorization"] = "bearer " + token;
+              axios
+                .delete(DEFAULT_SERVER_URL + url, formData, config)
+                .then(res2 => {
+                  console.log("response on DELETE", url, " : ", res);
+                  resolve(res2);
+                });
+            });
+          } else if (res.data.status === "OK") {
+            console.log("response on DELETE", url, " : ", res);
+            resolve(res);
+          } else {
+            reject(res);
+          }
         })
-    })
-}
+        .catch(reject => {
+          console.log(reject);
+        });
+    });
+  };
+  
